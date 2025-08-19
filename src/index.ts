b0bd1429142f6c6bf0d4ac5e7cd8eda80288e5ea
Babar <babarsmoke24@gmail.com>
2025-08-19 21:57:45 +0530

    chore  : Dotenv Added

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 const client = new Client({
     host: process.env.DB_HOST,
-    port: process.envDB_PORT,
+    // port: parseInt(process.env.DB_PORT),
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
 });
 client
     .connect()
