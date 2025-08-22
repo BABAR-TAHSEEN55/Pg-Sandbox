fddb7528ca12f090e87a53eba74c5502b4ed16c6
Babar <babarsmoke24@gmail.com>
2025-08-22 21:01:42 +0530

    Learnt about querying Data

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const client = new Client({
 client
     .connect()
     .then(() => console.log("Connected to DB"))
-    .catch((err) => console.log("Erro while connecting", err));
+    .catch((err) => console.log("Error while connecting", err));
 
 const CreateUserTable = async () => {
     const result = await client.query(`
@@ -25,4 +25,44 @@ created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP)
 `);
     console.log(result);
 };
-CreateUserTable();
+// CreateUserTable();
+const InsertQuery = async (
+    username: string,
+    email: string,
+    password: string,
+) => {
+    const InsertQry =
+        "INSERT INTO USERS1 (username,email,password) values($1,$2,$3)";
+    const values = [username, email, password];
+    try {
+        const InsertedQuery = await client.query(InsertQry, values);
+        console.log("Query has been inserted successfully", InsertedQuery);
+    } catch (error) {
+        console.log("Error Inserting ", error);
+    } finally {
+        await client.end();
+    }
+};
+// InsertQuery("Abdullah", "abdullah@gmail.com", "Abdullah");
+
+const GetData = async (email: string) => {
+    const Query = "SELECT * FROM USERS1 WHERE EMAIL=$1";
+    try {
+        const result = await client.query(Query, [email]);
+        if (result.rows.length > 0) {
+            console.log("User Found", result.rows[0]);
+            // return result.rows[0];
+        } else {
+            console.log("No User Found ");
+            return null;
+        }
+    } catch (error) {
+        console.log("Error while querying", error);
+    } finally {
+        await client.end();
+    }
+};
+
+// GetData("super@gmail.com");
+
+//TODO: : Peform SQL Injection
